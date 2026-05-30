0f6cb44715e986afd760a4874833e98081649bea
Rafael Novais de Miranda <56369172+rafaelnm90@users.noreply.github.com>
2026-05-30 19:04:01 -0300

    Update ai_service.py

--- ai_service.py
+++ ai_service.py
@@ -7,9 +7,13 @@ import time
 
 EXIBIR_LOGS = True
 MODELOS_CASCATA = [
-    'gemini-3.1-pro-preview', 
-    'gemini-3-flash-preview', 
-    'gemini-3.1-flash-lite-preview'
+    "gemini-3.1-pro-preview",
+    "gemini-3.5-flash",
+    "gemini-3-flash-preview",
+    "gemini-3.1-flash-lite-preview",
+    "gemini-2.5-pro",
+    "gemini-2.5-flash",
+    "gemini-2.5-flash-lite"
 ]
 
 def analyze_food_image(image_bytes, mime_type="image/jpeg", api_key=None):
